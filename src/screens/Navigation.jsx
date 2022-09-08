8fbff88d7693288adb238e4a9dceeeae7ab6efe4
advanced-user <stolbetskiy.yura@gmail.com>
2022-09-08 21:12:47 +0200

    folder structure changed

--- src/screens/Navigation.jsx
+++ src/screens/Navigation.jsx
@@ -5,9 +5,9 @@ import {DialogsScreen} from "./Dialogs/Dialogs";
 import {SosScreen} from "./Sos/Sos";
 import HomeScreenContainer from "./Home/HomeContainer";
 import {DocumentsScreen} from "./Documents/Documents";
-import {NotificationsScreen} from "./Dialogs/Notifications/Notifications";
-import {ServicesScreen} from "./Dialogs/Services/Services";
-import {MenuScreen} from "./Dialogs/Menu/Menu";
+import {NotificationsScreen} from "./Notifications/Notifications";
+import {ServicesScreen} from "./Services/Services";
+import {MenuScreen} from "./Menu/Menu";
 
 const Stack = createNativeStackNavigator();
 
@@ -16,13 +16,15 @@ export const Navigation = () => {
         <NavigationContainer>
             <Stack.Navigator screen screenOptions={{headerShown: false}}>
                 <Stack.Screen name="Home" component={HomeScreenContainer} options={{title: 'Home'}} />
+                <Stack.Screen name="SOS" component={SosScreen} options={{title: 'SOS'}}/>
+
+                <Stack.Screen name="Documents" component={DocumentsScreen} options={{title: 'Documents'}}/>
+                <Stack.Screen name="Services" component={ServicesScreen} options={{title: 'Services'}}/>
+                <Stack.Screen name="Notifications" component={NotificationsScreen} options={{title: 'Notifications'}}/>
+                <Stack.Screen name="Menu" component={MenuScreen} options={{title: 'Menu'}}/>
+
                 <Stack.Screen name="Dialogs" component={DialogsScreen} options={{title: 'Dialogs'}}/>
                 <Stack.Screen name="Dialog" component={DialogScreen} options={{title: 'Dialog'}}/>
-                <Stack.Screen name="Documents" component={DocumentsScreen} options={{title: 'Documents'}}/>
-                <Stack.Screen name="Notifications" component={NotificationsScreen} options={{title: 'Notifications'}}/>
-                <Stack.Screen name="Services" component={ServicesScreen} options={{title: 'Services'}}/>
-                <Stack.Screen name="Menu" component={MenuScreen} options={{title: 'Menu'}}/>
-                <Stack.Screen name="SOS" component={SosScreen} options={{title: 'SOS'}}/>
             </Stack.Navigator>
         </NavigationContainer>
     )
