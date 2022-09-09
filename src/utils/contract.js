a23a79d83dc91e3dc5bf25de7bc3856eb1f520f7
Alex Pitenko <oleksii.p@itsyndicate.org>
2022-09-09 03:43:58 +0200

    added contract functions

---
+++ src/utils/contract.js
@@ -0,0 +1,39 @@
+const nearAPI = require("near-api-js");
+const { connect, Contract } = nearAPI;
+
+const nearConnection = await connect(connectionConfig);
+const account = await nearConnection.account("zvyazok.testnet");
+
+const contract = new Contract(
+    account,
+    "zvyazok.testnet",
+    {
+        viewMethods: ["register_account", "send_message"],
+        sender: account,
+    }
+);
+
+let registerAccount = async (phoneNumber) => {
+    await contract.register_account(
+        {
+            args: {
+                phone_number: phoneNumber
+            },
+        }
+    );
+};
+
+let sendMessage = async (from, to, message) => {
+    await contract.register_account(
+        {
+            args: {
+                message: message,
+                sender_phone_number: from,
+                receiver_phone_number: to
+            },
+        }
+    );
+};
+
+export default registerAccount;
+export default contract;
